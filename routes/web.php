d52525c0c4fd80e0266917274ce9df9ec8ae4ee7
“[mahsa <mahsaabarghani@gmail.com>
2024-09-05 22:01:33 +0330

    MainPageController deleted

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MainPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
@@ -19,4 +20,5 @@ Route::middleware('auth')->group(function () {
 
 
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
